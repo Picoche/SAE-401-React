ffff4e96fb651a023df943a871833fa260a2335a
Picoche <hombert.fabien@gmail.com>
2023-04-07 17:45:05 +0200

    test fix config

--- Panier d'Antan/vite.config.js
+++ Panier d'Antan/vite.config.js
@@ -5,6 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(() => ({
   esbuild: {
     loader: "jsx",
+    jsxFactory: 'React.createElement',
+    jsxFragment: 'React.Fragment',
     include: /src\/.*\.jsx?$/,
     // loader: "tsx",
     // include: /src\/.*\.[tj]sx?$/,
