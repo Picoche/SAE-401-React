0f3d61b6957b6e7c05ebefa816945e288ca6f254
Picoche <hombert.fabien@gmail.com>
2023-04-08 22:22:52 +0200

    transition api geocoding

--- Panier d'Antan/src/components/Map.js
+++ Panier d'Antan/src/components/Map.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useContext } from "react";
 import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useGeolocated } from "react-geolocated";
-import { OpenStreetMapProvider } from "leaflet-geosearch";
 
 import UserContext from "../UserContext";
 
@@ -12,8 +11,79 @@ function SetMapView({ position, zoom }) {
   return null;
 }
 
-function MapParams({ position, zoom }) {
-  return (
+export default function Map() {
+  const { userContext } = useContext(UserContext);
+
+  const [markers, setMarkers] = useState([]);
+  const [boutiques, setBoutiques] = useState([]);
+  const [position, setPosition] = useState([43.604652, 1.444209]);
+  const [zoom, setZoom] = useState(13);
+
+  const url = "https://api.geocodify.com/v2/geocode?api_key=";
+  const apiKey = "4cdfd72e4860b817f9279f98e9d41f271fa9793b&q=";
+
+  const { coords, isGeolocationAvailable, isGeolocationEnabled } =
+    useGeolocated({
+      positionOptions: {
+        enableHighAccuracy: false,
+      },
+      watchPosition: false,
+      userDecisionTimeout: 5000,
+    });
+
+  const getBoutiques = () => {
+    fetch("http://panier-antan.mmicastres.fr/api/boutiques")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setBoutiques(data);
+      });
+  };
+
+  const getMarkers = () => {
+    boutiques.forEach((boutique) => {
+      fetch(url + apiKey + boutique.adresse_boutique).then((response) => {
+        response.json().then((data) => {
+          console.log(data);
+          setMarkers([
+            ...markers,
+            {
+              id_boutique: boutique.id_boutique,
+              nom_boutique: boutique.nom_boutique,
+              position: [data.response.bbox[1], data.response.bbox[0]],
+            },
+          ]);
+        });
+      });
+    });
+    console.log(markers);
+  };
+
+  useEffect(() => {
+    if (coords) {
+      fetch(url + apiKey + userContext.adresse).then((response) => {
+        response.json().then((data) => {
+          console.log(data);
+          setPosition([data.response.bbox[1], data.response.bbox[0]]);
+        });
+      });
+    }
+    console.log(position);
+  }, [coords]);
+
+  useEffect(() => {
+    getBoutiques();
+  }, []);
+
+  useEffect(() => {
+    getMarkers();
+  }, [boutiques]);
+
+  return !isGeolocationAvailable ? (
+    <div>Votre navigateur ne supporte pas la géolocalisation</div>
+  ) : !isGeolocationEnabled ? (
+    <div>Veuillez activer la géolocalisation</div>
+  ) : coords ? (
     <MapContainer
       center={position}
       zoom={zoom}
@@ -30,91 +100,78 @@ function MapParams({ position, zoom }) {
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
       </Marker>
-      <MapMarkers />
+      {markers.map((marker, index) => {
+        return (
+          <Marker position={marker.position} key={index}>
+            <Popup>
+              <h3>{marker.nom_boutique}</h3>
+            </Popup>
+          </Marker>
+        );
+      })}
     </MapContainer>
-  );
-}
-
-function MapMarkers() {
-  const [markerPosition, setMarkerPosition] = useState([]);
-  const [boutiques, setBoutiques] = useState([]);
-
-  const provider = new OpenStreetMapProvider({
-    params: {
-      email: "hombert.fabien@gmail.com",
-    },
-  });
-
-  const getBoutiques = () => {
-    fetch("http://panier-antan.mmicastres.fr/api/boutiques")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setBoutiques(data);
-        data.forEach((boutique) => {
-          provider
-            .search({ query: boutique.adresse_boutique })
-            .then((result) => {
-              console.log(result);
-              setMarkerPosition(
-                { id_boutique: boutique.id_boutique },
-                { position: [result[0].y, result[0].x] }
-              );
-            });
-        });
-      });
-  };
-
-  useEffect(() => {
-    getBoutiques();
-  }, []);
-
-  return markerPosition.map((marker) => {
-    return (
-      <Marker position={marker.position} key={marker.id_boutique}></Marker>
-    );
-  });
-}
-
-export default function Map() {
-  const { userContext } = useContext(UserContext);
-
-  const [position, setPosition] = useState([43.604652, 1.444209]);
-  const [zoom, setZoom] = useState(13);
-
-  const provider = new OpenStreetMapProvider({
-    params: {
-      email: "hombert.fabien@gmail.com",
-    },
-  });
-
-  const { coords, isGeolocationAvailable, isGeolocationEnabled } =
-    useGeolocated({
-      positionOptions: {
-        enableHighAccuracy: false,
-      },
-      watchPosition: false,
-      userDecisionTimeout: 5000,
-    });
-
-  useEffect(() => {
-    if (coords) {
-      provider.search({ query: userContext.adresse }).then((result) => {
-        console.log(result);
-        setPosition([result[0].y, result[0].x]);
-      });
-      console.log(coords);
-      console.log(position);
-    }
-  }, [coords]);
-
-  return !isGeolocationAvailable ? (
-    <div>Votre navigateur ne supporte pas la géolocalisation</div>
-  ) : !isGeolocationEnabled ? (
-    <div>Veuillez activer la géolocalisation</div>
-  ) : coords ? (
-    <MapParams position={position} zoom={zoom} />
   ) : (
     <div>En attente des données de géolocalisation&hellip; </div>
   );
 }
+
+// function MapParams({ position, zoom }) {
+//   return (
+//     <MapContainer
+//       center={position}
+//       zoom={zoom}
+//       scrollWheelZoom={true}
+//       style={{ height: 500 }}
+//     >
+//       <TileLayer
+//         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//       />
+//       <SetMapView position={position} zoom={zoom} />
+//       <Marker position={position}>
+//         <Popup>
+//           A pretty CSS3 popup. <br /> Easily customizable.
+//         </Popup>
+//       </Marker>
+//       <MapMarkers />
+//     </MapContainer>
+//   );
+// }
+
+// export default function Map() {
+//   const { userContext } = useContext(UserContext);
+
+//   const url = "https://api.geocodify.com/v2/geocode?api_key=";
+//   const apiKey = "4cdfd72e4860b817f9279f98e9d41f271fa9793b&q=";
+
+//   const { coords, isGeolocationAvailable, isGeolocationEnabled } =
+//     useGeolocated({
+//       positionOptions: {
+//         enableHighAccuracy: false,
+//       },
+//       watchPosition: false,
+//       userDecisionTimeout: 5000,
+//     });
+
+//   useEffect(() => {
+//     if (coords) {
+//       fetch(url + apiKey + userContext.adresse).then((response) => {
+//         response.json().then((data) => {
+//           console.log(data);
+//           setPosition([data.response.bbox[1], data.response.bbox[0]]);
+//         });
+//       });
+//     }
+//     console.log(position);
+//   }, [coords]);
+
+//   return !isGeolocationAvailable ? (
+//     <div>Votre navigateur ne supporte pas la géolocalisation</div>
+//   ) : !isGeolocationEnabled ? (
+//     <div>Veuillez activer la géolocalisation</div>
+//   ) : coords ? (
+//     <MapParams position={position} zoom={zoom} />
+//   ) : (
+//     <div>En attente des données de géolocalisation&hellip; </div>
+//   );
+// }
