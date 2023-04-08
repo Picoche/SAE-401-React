07adcb6fee756a1927ef3c98c773359be6225194
Picoche <hombert.fabien@gmail.com>
2023-04-08 12:40:26 +0200

    fixed regex for file loading

--- Panier d'Antan/vite.config.js
+++ Panier d'Antan/vite.config.js
@@ -1,34 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from 'fs/promises';
-
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
   esbuild: {
-    jsxFactory: 'React.createElement',
-  jsxFragment: 'React.Fragment',
     loader: "jsx",
-    jsxFactory: 'React.createElement',
-    jsxFragment: 'React.Fragment',
-    include: /src\/.*\.jsx?$/,
+    include: /src\/.*\.(js|jsx?)?$/,
     // loader: "tsx",
     // include: /src\/.*\.[tj]sx?$/,
     exclude: [],
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [
-        {
-          name: "load-js-files-as-jsx",
-          setup(build) {
-            build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
-              loader: 'jsx',
-              contents: await fs.readFile(args.path, "utf8"),
-            }));
-          },
-        },
-      ],
-    },
   },
 }));
