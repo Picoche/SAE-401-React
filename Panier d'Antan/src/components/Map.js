0a698458c9de7376090bf20fee09ca76ba7611ea
Picoche <hombert.fabien@gmail.com>
2023-04-09 14:10:51 +0200

    markers fonctionnels, popups next

--- Panier d'Antan/src/components/Map.js
+++ Panier d'Antan/src/components/Map.js
@@ -14,7 +14,6 @@ function SetMapView({ position, zoom }) {
 export default function Map() {
   const { userContext } = useContext(UserContext);
 
-  const [markers, setMarkers] = useState([]);
   const [boutiques, setBoutiques] = useState([]);
   const [position, setPosition] = useState([43.604652, 1.444209]);
   const [zoom, setZoom] = useState(13);
@@ -35,35 +34,14 @@ export default function Map() {
     fetch("http://panier-antan.mmicastres.fr/api/boutiques")
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
         setBoutiques(data);
       });
-  };
-
-  const getMarkers = () => {
-    boutiques.forEach((boutique) => {
-      fetch(url + apiKey + boutique.adresse_boutique).then((response) => {
-        response.json().then((data) => {
-          console.log(data);
-          setMarkers([
-            ...markers,
-            {
-              id_boutique: boutique.id_boutique,
-              nom_boutique: boutique.nom_boutique,
-              position: [data.response.bbox[1], data.response.bbox[0]],
-            },
-          ]);
-        });
-      });
-    });
-    console.log(markers);
   };
 
   useEffect(() => {
     if (coords) {
       fetch(url + apiKey + userContext.adresse).then((response) => {
         response.json().then((data) => {
-          console.log(data);
           setPosition([data.response.bbox[1], data.response.bbox[0]]);
         });
       });
@@ -75,15 +53,46 @@ export default function Map() {
     getBoutiques();
   }, []);
 
-  useEffect(() => {
-    getMarkers();
-  }, [boutiques]);
-
   return !isGeolocationAvailable ? (
     <div>Votre navigateur ne supporte pas la géolocalisation</div>
   ) : !isGeolocationEnabled ? (
     <div>Veuillez activer la géolocalisation</div>
   ) : coords ? (
+    <BuildMap position={position} zoom={zoom} boutiques={boutiques} />
+  ) : (
+    <div>En attente des données de géolocalisation&hellip; </div>
+  );
+}
+
+function BuildMap({ position, zoom, boutiques }) {
+  const [markers, setMarkers] = useState([]);
+
+  useEffect(() => {
+    const url = "https://api.geocodify.com/v2/geocode?api_key=";
+    const apiKey = "4cdfd72e4860b817f9279f98e9d41f271fa9793b&q=";
+
+    const getMarkers = () => {
+      boutiques.forEach((boutique) => {
+        fetch(url + apiKey + boutique.adresse_boutique).then((response) => {
+          response.json().then((data) => {
+            setMarkers((prevMarkers) => [
+              ...prevMarkers,
+              {
+                info: {
+                  boutique,
+                },
+                position: [data.response.bbox[1], data.response.bbox[0]],
+              },
+            ]);
+          });
+        });
+      });
+    };
+
+    getMarkers();
+  }, [boutiques]);
+
+  return (
     <MapContainer
       center={position}
       zoom={zoom}
@@ -95,25 +104,56 @@ export default function Map() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <SetMapView position={position} zoom={zoom} />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-      {markers.map((marker, index) => {
-        return (
-          <Marker position={marker.position} key={index}>
-            <Popup>
-              <h3>{marker.nom_boutique}</h3>
-            </Popup>
-          </Marker>
-        );
-      })}
+      {markers.map((marker, index) => (
+        <Marker position={marker.position} key={index}>
+          <Popup>
+            <h3>{marker.info.boutique.nom_boutique}</h3>
+          </Popup>
+        </Marker>
+      ))}
     </MapContainer>
-  ) : (
-    <div>En attente des données de géolocalisation&hellip; </div>
   );
 }
+
+// function Markers({ boutiques }) {
+//   const [markers, setMarkers] = useState([]);
+
+//   const url = "https://api.geocodify.com/v2/geocode?api_key=";
+//   const apiKey = "4cdfd72e4860b817f9279f98e9d41f271fa9793b&q=";
+
+//   const getMarkers = () => {
+//     boutiques.forEach((boutique) => {
+//       fetch(url + apiKey + boutique.adresse_boutique).then((response) => {
+//         response.json().then((data) => {
+//           console.log(data);
+//           setMarkers([
+//             ...markers,
+//             {
+//               info: {
+//                 boutique,
+//               },
+//               position: [data.response.bbox[1], data.response.bbox[0]],
+//             },
+//           ]);
+//         });
+//       });
+//     });
+//   };
+
+//   useEffect(() => {
+//     getMarkers();
+//   }, [boutiques]);
+
+//   return markers.map((marker, index) => {
+//     return (
+//       <Marker position={marker.position} key={index}>
+//         <Popup>
+//           <h3>{marker.info.boutique.nom_boutique}</h3>
+//         </Popup>
+//       </Marker>
+//     );
+//   });
+// }
 
 // function MapParams({ position, zoom }) {
 //   return (
