531fc33642916a7500ab523a71a79918bde7e9ee
Picoche <hombert.fabien@gmail.com>
2023-04-09 19:13:24 +0200

    vite config

--- Panier d'Antan/vite.config.js
+++ Panier d'Antan/vite.config.js
@@ -1,13 +1,28 @@
+import fs from "fs/promises";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig(() => ({
   esbuild: {
     loader: "jsx",
-    include: /src\/.*\.(js|jsx?)?$/,
+    include: /src\/.*\.jsx?$/,
     // loader: "tsx",
     // include: /src\/.*\.[tj]sx?$/,
     exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: { ".js": "jsx" },
+      plugins: [
+        {
+          name: "load-js-files-as-jsx",
+          setup(build) {
+            build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
+              loader: "jsx",
+              contents: await fs.readFile(args.path, "utf8"),
+            }));
+          },
+        },
+      ],
+    },
   },
 }));
