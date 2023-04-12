24983f49e4896b13986eab4f261c821570f24702
Picoche <hombert.fabien@gmail.com>
2023-04-12 14:42:12 +0200

    redirection après log in

--- Panier d'Antan/src/components/FormLogin.js
+++ Panier d'Antan/src/components/FormLogin.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useCallback } from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -11,15 +11,20 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import UserContext from "../UserContext";
 
 export default function SignIn() {
+  const navigate = useNavigate();
   const { userContext, setUserContext } = useContext(UserContext);
 
   const [data, setData] = useState({});
   const [logInFeedback, setLogInFeedback] = useState(null);
+
+  const logInSuccess = useCallback(() => {
+    navigate("/boutiques");
+  }, []);
 
   const updateData = (event) => {
     setData({
@@ -44,8 +49,8 @@ export default function SignIn() {
         if (data.status === 1) {
           localStorage.setItem("userContext", JSON.stringify(data.user));
           setUserContext(data.user);
+          logInSuccess();
         } else {
-          setData({});
           setLogInFeedback(
             "Votre email ou votre mot de passe est incorrect, veuillez réessayer."
           );
