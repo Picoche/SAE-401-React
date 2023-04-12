fd91432fcfa73be9fa024dfb3ce72831c7d94328
Your Name <caliacoulibaly@gmail.com>
2023-04-12 14:42:50 +0200

    Update headers et syles pages

--- Panier d'Antan/src/views/InfoComView.js
+++ Panier d'Antan/src/views/InfoComView.js
@@ -1,5 +1,14 @@
 import { Button, Typography, createTheme, ThemeProvider } from "@mui/material";
 import React from "react";
+import "@fontsource/unbounded";
+import { Link } from "react-router-dom";
+import "../styles.css";
+import shop1 from "../assets/shop1.svg";
+import shop2 from "../assets/shop_bandeau.svg";
+import shop3 from "../assets/question1.svg";
+import shop4 from "../assets/support2.svg";
+import shop5 from "../assets/commerce3.svg";
+
 
 
 export default function InfoComView() {
@@ -14,36 +23,78 @@ export default function InfoComView() {
     },
   });
   return (
-    <div>
-      <div className="divAccueilNC" style={{
+    <div style={{
+      backgroundColor: "#FFFF",
+      textAlign: "center",
+      alignItems: "center",
+    }}>
+      <div style={{
         display: "flex",
-        flexDirection:"column"
+        flexDirection: "collumn"
       }}>
-        <ThemeProvider theme={titre}>
-          <Typography variant="h5" sx={{ fontWeight: "bold", margin: "20px 15px"}}>
-            Vous êtes commerçant et voulez vous inscrire en tant que tel ? C'est ici !
-          </Typography>
-        </ThemeProvider>
-        <Button variant="contained" className="button">
-          COMMERCANT
-        </Button>
-      </div>
-      <div className="h2DevComNC">
-        <h3>Comment ca marche ?</h3>
-        <div className="blocDevComNC">
-          <p>
-            En vous inscrivant en tant que commerçant, vous aurez la possibilité
-            de référencer votre (ou vos) boutique(s) sur notre application !
-            Pour ce faire, il vous suffit de remplir le formulaire situé en pied
-            de page. Une fois envoyé, notre équipe se chargera des derniers
-            détails, et vous enverra un email de confirmation de création de
-            compte commerçant vous indiquant les identifiants qui vous
-            permettront de gérer votre (ou vos) boutique(s).
-          </p>
+        <div className="divAccueilNC">
+          <img src={shop2} className="imgAccueilNC"></img>
         </div>
+        <div style={{
+          display: "flex",
+          flexDirection: "column"
+        }}>
+          <ThemeProvider theme={titre}>
+            <Typography variant="h5" sx={{ fontWeight: "bold", margin: "20px 15px" }}>
+              Vous êtes commerçant et voulez vous inscrire en tant que tel ? C'est ici !
+            </Typography>
+          </ThemeProvider>
+          <Link>
+            <Button variant="contained" className="button" style={{
+              borderRadius: "30%",
+              margin: "20px 15px",
+            }}>
+              <Typography>COMMERCANT</Typography>
+            </Button>
+          </Link>
+        </div>
+        <div>
+        </div>
+        <img src={shop1} className="imgAccueilNC"></img>
       </div>
+
+      <div style={{
+         backgroundColor: "#F5F5F5",
+         display: "flex",
+         flexDirection: "row"
+      }}>
+       
+        <div>
+        <ThemeProvider theme={titre}>
+        <Typography variant="h5" sx={{ fontWeight: "bold", margin: "20px 15px", color:"#FF9914" }}>
+        Comment ca marche ?
+            </Typography>
+        </ThemeProvider>
+
+          <div className="blocDevComNC">
+          <Typography>
+                          En vous inscrivant en tant que commerçant, vous aurez la possibilité
+              de référencer votre (ou vos) boutique(s) sur notre application !
+              Pour ce faire, il vous suffit de remplir le formulaire situé en pied
+              de page. Une fois envoyé, notre équipe se chargera des derniers
+              détails, et vous enverra un email de confirmation de création de
+              compte commerçant vous indiquant les identifiants qui vous
+              permettront de gérer votre (ou vos) boutique(s).
+            </Typography>
+            </div>
+        </div>
+        <img src={shop3} className="imgAccueilNC"></img>
+      </div>
+
+      {/*  */}
       <div className="h2DevComNC">
-        <h3>D'accord, mais concrètement, qu'est ce que j'y gagne ?</h3>
+      <img src={shop4} className="imgAccueilNC"></img>
+
+      <ThemeProvider theme={titre}>
+        <Typography variant="h5" sx={{ fontWeight: "bold", margin: "20px 15px", color:"#FF9914" }}>
+        D'accord, mais concrètement, qu'est ce que j'y gagne ?
+        </Typography>
+        </ThemeProvider>
         <div className="blocDevComNC">
           <p>
             Comme vous l’avez peut-être lu sur notre page d’accueil, le but de
@@ -56,6 +107,7 @@ export default function InfoComView() {
             renouer des liens avec des commerces de qualité.
           </p>
         </div>
+
       </div>
       <div className="h2DevComNC">
         <h3>Je ne suis pas très à l'aise avec ce type d'outils ?</h3>
@@ -86,6 +138,7 @@ export default function InfoComView() {
         </Button>
         <div className="blocDevComNC"></div>
       </div>
-    </div>
+    </div >
+
   );
 }
